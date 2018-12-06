dad3c1f95de98a982e21d7e6589eeca9bb241062
Joseph McAllister <joseph.l.mcallister@gmail.com>
2018-12-05 22:28:36 -0500

    add flush

--- AWS_models/modified_action_size.py
+++ AWS_models/modified_action_size.py
@@ -183,6 +183,7 @@ def main(plot_scores=True):
 #     print(agent.trained_frames)
     while True:
         print(curr_episode)
+        sys.stdout.flush()
 
         episodes.append(num_episodes)
         num_episodes += 1
