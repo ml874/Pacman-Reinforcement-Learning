6369f5f30f860d75e8ee86cf3fec47639024da6c
Joseph McAllister <joseph.l.mcallister@gmail.com>
2018-12-05 19:07:50 -0500

    modify performance test

--- performance_tests/performance_test0.py
+++ performance_tests/performance_test0.py
@@ -1,12 +1,12 @@
 # DQN Agent for the MsPacman
 # it uses Neural Network to approximate q function and replay memory & target q network
 
-weight_path = "../AWS_models/saved-weights-1/first_aws_model--4000"
+weight_path = "../AWS_models/saved-weights-first-1k-6k/first_aws_model--10000"
 
 class TEST_DQNAgent:
     def __init__(self, state_size, action_size):
         # if you want to see MsPacman learning, then change to True
-        self.render = False
+        self.render = True
         self.load_model = True
         self.epsilon = 0.1
 
